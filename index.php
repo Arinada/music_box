bc1560a82ecd6ef89802c3e3b6eb168c40a73236
Arinada <a.potekhina@g.nsu.ru>
2021-12-03 16:56:05 +0700

    Change constants and namespaces

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+ //require 'MainPage.php';
    require_once 'PSR4AutoloaderClass.php';
 
    const CONTROLLER_DIR_PATH = __DIR__ . '/controllers';
+   const VIEW_DIR_PATH = __DIR__ . '/views';
+
    const NAMESPACE_APP_PREFIX = 'MusicBoxApp\\';
    const NAMESPACE_CONTROLLER_PREFIX = NAMESPACE_APP_PREFIX . 'Controllers\\';
+   const NAMESPACE_VIEW_PREFIX = NAMESPACE_APP_PREFIX . 'Views\\';
 
    $loader = new Example\Psr4AutoloaderClass();
    $loader->register();
@@ -12,6 +16,7 @@
    $loader->addNamespace(NAMESPACE_APP_PREFIX, __DIR__);
    $loader->addNamespace(NAMESPACE_APP_PREFIX . 'Autoloader', __DIR__);
    $loader->addNamespace(NAMESPACE_CONTROLLER_PREFIX, CONTROLLER_DIR_PATH);
+   $loader->addNamespace(NAMESPACE_VIEW_PREFIX, VIEW_DIR_PATH);
 
    $routes_obj = new MusicBoxApp\Routes();
    $routes = $routes_obj->getRoutes();

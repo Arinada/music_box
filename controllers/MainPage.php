93533f33604d7d3695bc5e40e6c4fd8d8154e6cc
Arinada <a.potekhina@g.nsu.ru>
2021-12-03 16:53:08 +0700

    Call main page template in controller
    add function callView().

--- controllers/MainPage.php
+++ controllers/MainPage.php
@@ -4,9 +4,20 @@ namespace MusicBoxApp\Controllers;
 
 class MainPage
 {
-    public function __construct()
+    private string $view_name;
+
+    public function __construct($view_name)
     {
-        echo "Мы в контроллере MAIN PAGE";
+        $this->view_name = $view_name;
+        $this->callView();
+        //NAMESPACE_VIEW_PREFIX . $view_name;
+       // echo "Мы в контроллере MAIN PAGE";
+    }
+
+    private function callView()
+    {
+        $authorize = null;
+        include VIEW_DIR_PATH . '/templates/' .  $this->view_name . '.php';
     }
 
     public function search($num)

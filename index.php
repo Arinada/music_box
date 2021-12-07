b58bc163ff042ec2ef99ee3bec50e7630818e8af
Arinada <a.potekhina@g.nsu.ru>
2021-12-07 13:55:31 +0700

    Add namespace to classloader

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
    const CONTROLLER_DIR_PATH = __DIR__ . '/controllers';
    const VIEW_DIR_PATH = __DIR__ . '/views';
+   const AUDIO_RECORDS_DIR = __DIR__ . '/audio_records/';
 
    const NAMESPACE_APP_PREFIX = 'MusicBoxApp\\';
    const NAMESPACE_CONTROLLER_PREFIX = NAMESPACE_APP_PREFIX . 'Controllers\\';
@@ -13,9 +14,10 @@
    $loader->register();
 
    $loader->addNamespace(NAMESPACE_APP_PREFIX, __DIR__);
-   $loader->addNamespace(NAMESPACE_APP_PREFIX . 'Autoloader', __DIR__);
+   $loader->addNamespace(NAMESPACE_APP_PREFIX . 'Routes\\', __DIR__ . '/route');
+   //$loader->addNamespace(NAMESPACE_APP_PREFIX . 'Autoloader', __DIR__);
    $loader->addNamespace(NAMESPACE_CONTROLLER_PREFIX, CONTROLLER_DIR_PATH);
    $loader->addNamespace(NAMESPACE_VIEW_PREFIX, VIEW_DIR_PATH);
 
-   $router = new MusicBoxApp\Router();
+   $router = new MusicBoxApp\Routes\Router();
    $router->callRoute();

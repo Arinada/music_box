977192ba446dca88e2523c8d1594ad28a56155cd
Arinada <a.potekhina@g.nsu.ru>
2021-12-07 16:11:06 +0700

    Change structure to call view

--- controllers/MainPage.php
+++ controllers/MainPage.php
@@ -2,34 +2,22 @@
 
 namespace MusicBoxApp\Controllers;
 
+use MusicBoxApp\Views as Views;
+
 class MainPage
 {
     private string $view_name;
 
     public function __construct($view_name)
     {
-        $this->normalizeViewName($view_name);
+        $this->view_name = $view_name;
         $this->callView();
-        //NAMESPACE_VIEW_PREFIX . $view_name;
-        // echo "Мы в контроллере MAIN PAGE";
     }
 
     private function callView()
     {
-        $authorized = null;
-        include VIEW_DIR_PATH . '/templates/' . $this->view_name . '.php';
-    }
-
-    private function normalizeViewName($view_name)
-    {
-        $normal_view_name = strtolower($view_name[0]);
-        for ($i = 1; $i < mb_strlen($view_name); $i++) {
-            if (ctype_upper($view_name[$i])) {
-                $normal_view_name = $normal_view_name . '_';
-            }
-            $normal_view_name = $normal_view_name . strtolower($view_name[$i]);
-        }
-        $this->view_name = $normal_view_name;
+        $view = new Views\View();
+        $view->render($this->view_name);
     }
 
     public function search($num)

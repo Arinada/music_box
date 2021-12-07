3933990bb44bef8c3f73aa4a517e53f024b5d17a
Arinada <a.potekhina@g.nsu.ru>
2021-12-07 14:52:43 +0700

    Add function to convert template's name to lowercase

--- controllers/MainPage.php
+++ controllers/MainPage.php
@@ -8,7 +8,7 @@ class MainPage
 
     public function __construct($view_name)
     {
-        $this->view_name = $view_name;
+        $this->normalizeViewName($view_name);
         $this->callView();
         //NAMESPACE_VIEW_PREFIX . $view_name;
         // echo "Мы в контроллере MAIN PAGE";
@@ -18,6 +18,18 @@ class MainPage
     {
         $authorized = null;
         include VIEW_DIR_PATH . '/templates/' . $this->view_name . '.php';
+    }
+
+    private function normalizeViewName($view_name)
+    {
+        $normal_view_name = strtolower($view_name[0]);
+        for ($i = 1; $i < mb_strlen($view_name); $i++) {
+            if (ctype_upper($view_name[$i])) {
+                $normal_view_name = $normal_view_name . '_';
+            }
+            $normal_view_name = $normal_view_name . strtolower($view_name[$i]);
+        }
+        $this->view_name = $normal_view_name;
     }
 
     public function search($num)

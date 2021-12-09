0daae73a36471d77725ec6577c91ae9693a51122
Arinada <a.potekhina@g.nsu.ru>
2021-12-09 14:22:35 +0700

    Delete unnecessary symbol

--- views/templates/blocks/audio_record.php
+++ views/templates/blocks/audio_record.php
@@ -1,5 +1,5 @@
 <div class="audio-record-wrapper">
     <audio controls class="audio">
-        <source src="<?php echo $path_to_audio; ?> type="audio/mpeg">
+        <source src=<?php echo $path_to_audio; ?>>
     </audio>
 </div>

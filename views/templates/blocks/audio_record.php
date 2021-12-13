758c5a25ac4eae6bb64bedef2edbd863f6e13cde
Arinada <a.potekhina@g.nsu.ru>
2021-12-13 14:22:16 +0700

    Add wrapper "div-audio-wrapper"

--- views/templates/blocks/audio_record.php
+++ views/templates/blocks/audio_record.php
@@ -1,6 +1,8 @@
-<div class="div-audio-wrapper">
-    <p class="audio-title"><?php echo $audio_name; ?></p>
-    <audio class="audio"  title="dfsdf" controls>
-        <source src=<?php echo $path_to_audio; ?> type="audio/mp3">
-    </audio>
+<div class="audio-wrapper" style="height: 25%">
+    <div class="div-audio-wrapper">
+        <p class="audio-title"><?php echo $audio_name; ?></p>
+        <audio class="audio" controls>
+            <source src=<?php echo $path_to_audio; ?> type="audio/mp3">
+        </audio>
+    </div>
 </div>

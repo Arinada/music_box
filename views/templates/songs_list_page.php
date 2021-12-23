04c807c7fcf385682563447e9ce23310bdfe2e7f
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 14:45:04 +0700

    Add html code to common visual part of page with all list of songs in songs_list_page.html

---
+++ views/templates/songs_list_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MusicBox</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="../../js/search_songs.js"></script>
+    <script src="../../js/common_functions.js"></script>
+    <link rel="stylesheet" href="../css/main_page/styles.css">
+</head>
+<body>
+<?php
+include_once VIEW_DIR_PATH . '/templates/layouts/header.php';
+include_once VIEW_DIR_PATH . '/templates/blocks/sort_filters.php';
+?>
+</body>
+</html>

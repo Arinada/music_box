edd642972cb42f246260debc40a1d51e6831ed03
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 14:46:29 +0700

    Add html code for dropdown-list to sort songs on page with all list of songs in sort_filters.php

---
+++ views/templates/blocks/sort_filters.php
@@ -0,0 +1,21 @@
+<div class="sort-filters">
+    <div class="input-group-btn search-panel">
+        <button id="btn_sort_by_fields" type="button" class="btn btn-default" style="width: 40%; margin-left: 45%; margin-right: 40px"
+                data-toggle="dropdown">
+            <span id="search_concept">Sort by</span> <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+            <li><a>Name</a></li>
+            <li><a>Author</a></li>
+        </ul>
+    </div>
+    <div class="input-group-btn search-panel">
+        <button id="btn_sort_by_order" type="button"  data-toggle="dropdown">
+            <span id="search_concept">Default</span> <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+            <li><a>Increase</a></li>
+            <li><a>Decrease</a></li>
+        </ul>
+    </div>
+</div>

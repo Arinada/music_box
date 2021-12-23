aaa1b855f7ed47d9ef5c159feb259c80e19f82ee
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 18:24:35 +0700

    Add class SongsListPage in SongsListPage.php
    Class SongsListPage is controller to work with page with all list of songs

---
+++ controllers/SongsListPage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MusicBoxApp\Controllers;
+
+use MusicBoxApp\Models\Song;
+use MusicBoxApp\Views as Views;
+
+class SongsListPage
+{
+    private string $view_name;
+    private Views\View $view;
+
+    public function __construct($view_name)
+    {
+        $this->view_name = $view_name;
+        $this->view = new Views\View();
+    }
+
+    public function showSongsListPage()
+    {
+        $condition = null;
+        $parameter = null;
+        $page_number = 1;
+
+        $this->view->renderSongsListPage($this->view_name);
+
+        $song_model = new Song();
+        $songs_list = $song_model->getAllSongsBy($condition, $parameter);
+
+        $songsController = new SongsController();
+        $songs_list_per_page = $songsController->getSongsPerPage($page_number, $songs_list);
+        $handler = 'ShowSongsListOnPage';
+        $pagesCount = $songsController->getPagesCounter($songs_list);
+        $this->view->renderSongs($songs_list_per_page, $pagesCount, $handler);
+    }
+
+    public function showSongsListOnPage($params)
+    {
+        $page_number = $params['page'];
+        $condition = null;
+        $parameter = null;
+
+        $song_model = new Song();
+        $songs_list = $song_model->getAllSongsBy($condition, $parameter);
+
+        $songsController = new SongsController();
+        $songs_list_per_page = $songsController->getSongsPerPage($page_number, $songs_list);
+        $pagesCount = $songsController->getPagesCounter($songs_list);
+        $handler = 'ShowSongsListOnPage';
+        $this->view->renderSongs($songs_list_per_page, $pagesCount, $handler);
+    }
+
+}

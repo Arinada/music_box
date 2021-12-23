4e14c2d671a2672a5885ce8e265a4013d3cad643
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 14:35:21 +0700

    Add separate html code for additional information on the site in introduction_info.php

---
+++ views/templates/blocks/introduction_info.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3>Share your own records with users</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Listen <a class="nav-link" href="#">Popular playlists</a></h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+        <div class="col-sm-4">
+            <h3>Make your playlist</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </div>
+    </div>
+</div>

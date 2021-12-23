c6f555a9d402b1ae417e07cb314522890255ac00
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 14:40:29 +0700

    Add class SongsController to do some logic with list of songs

---
+++ controllers/SongsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MusicBoxApp\Controllers;
+
+use MusicBoxApp\Models\Model;
+
+class SongsController
+{
+    private int $songs_per_page = 5;
+
+    public function __construct()
+    {
+    }
+
+    public function getSongsPerPage($page_number, $songs_list): array
+    {
+        $start_ind = $this->songs_per_page * ($page_number - 1);
+        return array_slice($songs_list, $start_ind, $this->songs_per_page);
+    }
+
+    public function getPagesCounter($songs_list): int
+    {
+        $pagesCount = count($songs_list) / $this->songs_per_page;
+        return ceil($pagesCount);
+    }
+}

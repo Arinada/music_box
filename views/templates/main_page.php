4a9a554dbc070332b135aa9848e2abc1431663c3
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 14:32:48 +0700

    Include separate parts for html elements in main_page.php

--- views/templates/main_page.php
+++ views/templates/main_page.php
@@ -7,78 +7,16 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="../../js/search_panel.js"></script>
+    <script src="../../js/search_songs.js"></script>
+    <script src="../../js/common_functions.js"></script>
     <link rel="stylesheet" href="../css/main_page/styles.css">
 </head>
 <body>
 
-<div class="jumbotron text-center">
-    <div class="text-right" id="shift-right">
-        <?php
-        if ($authorized):
-            include_once VIEW_DIR_PATH . '/templates/blocks/view_song_element.php';
-        else:
-            include_once VIEW_DIR_PATH . '/templates/blocks/profile_buttons.php';
-        endif; ?>
-    </div>
-    <h1>MusicBox</h1>
-    <p>Listen to high quality music on MusicBox!</p>
-</div>
-
-<div class="container">
-    <div class="row">
-        <div class="col-sm-4">
-            <h3>Share your own records with users</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-        <div class="col-sm-4">
-            <h3>Listen <a class="nav-link" href="#">Popular playlists</a></h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-        <div class="col-sm-4">
-            <h3>Make your playlist</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-    </div>
-</div>
-<!-- search panel-->
-<!------ Include the above in your HEAD tag ---------->
-<div class="container" id="search_id">
-    <div class="row">
-        <div class="col-xs-8 col-xs-offset-2">
-            <div class="input-group">
-                <div class="input-group-btn search-panel">
-                    <button id="btn_filter_by" type="button" class="btn btn-default dropdown-toggle"
-                            data-toggle="dropdown">
-                        <span id="search_concept">Filter by</span> <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a>Contain</a></li>
-                        <li><a>Equal</a></li>
-                        <li><a>Start with</a></li>
-                    </ul>
-                </div>
-                <input type="hidden" name="search_param" value="all" id="search_param">
-                <input type="text" class="form-control" name="search_by" id="search_by" placeholder="Search term...">
-                <span class="input-group-btn">
-                    <button class="btn btn-default" onclick="showSearchValue()" type="button"><span
-                                class="glyphicon glyphicon-search"></span></button>
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
 <?php
-echo '<div class="audios-wrapper">';
-foreach ($songs_list as $song_data) {
-    $audio_name = $song_data['name'] . '     ' . $song_data['author'];
-    $path_to_audio = $song_data['path_on_server'];
-    include VIEW_DIR_PATH . '/templates/blocks/audio_record.php';
-}
-echo '</div>';
+include_once VIEW_DIR_PATH . '/templates/layouts/header.php';
+include_once VIEW_DIR_PATH . '/templates/blocks/introduction_info.php';
+include_once VIEW_DIR_PATH . '/templates/blocks/search_panel.php';
 ?>
 </body>
 </html>

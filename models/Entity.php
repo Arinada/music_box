5b7385aad840acf9c15299b9f87ca5bd59c4d6cc
Arinada <a.potekhina@g.nsu.ru>
2021-12-23 18:14:53 +0700

    Change class Entity in Entity.php
    Add destructor. Add object to communicate with database as field, add creating this object in constructor.

--- models/Entity.php
+++ models/Entity.php
@@ -2,44 +2,45 @@
 
 namespace MusicBoxApp\Models;
 
-class Entity implements EntityObj
+class Entity
 {
     protected static array $fields_description;
     protected static string $table_name = 'ENTITY';
+    protected DBClass $db_obj;
 
     public function __construct()
     {
         require_once 'config/config.php';
         $config = getConfig();
-        $db_obj = new DBClass();
-        $this->db_connection = $db_obj->getConnection($config);
-    }
+        $this->db_obj = new DBClass();
+        $this->db_obj->getConnection($config);
 
-    public function insert()
-    {
-        // TODO: Implement insert() method.
-    }
-
-    public function update()
-    {
-        // TODO: Implement update() method.
-    }
-
-    public function delete()
-    {
-        // TODO: Implement delete() method.
-    }
-
-    public function createEntity($db_obj)
-    {
-        if (!$db_obj->isTableExist(self::$table_name)) {
-            $query = $db_obj->createCreateTableQuery(self::$table_name, self::$fields_description);
-            $db_obj->execute($query);
+        if (!$this->db_obj->isTableExist(self::$table_name)) {
+            $query = $this->db_obj->createCreateTableQuery(self::$table_name, self::$fields_description);
+            $this->db_obj->execute($query);
         }
     }
 
-    public static function getAllSongs($db_obj): array
+    /*  public function createEntity($db_obj)
+      {
+          if (!$db_obj->isTableExist(self::$table_name)) {
+              $query = $db_obj->createCreateTableQuery(self::$table_name, self::$fields_description);
+              $db_obj->execute($query);
+          }
+      }*/
+
+    public static function getAllRows($db_obj): array
     {
-        return $db_obj->getAllRows('Song');
+        return $db_obj->getAllRows(self::$table_name);
+    }
+
+    public function __destruct()
+    {
+        $this->db_obj->closeConnection();
+    }
+
+    protected function getDbObj(): DBClass
+    {
+        return $this->db_obj;
     }
 }

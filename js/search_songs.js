a42d39c3ac1d22dd0248094f699282225ca2177e
Arinada <a.potekhina@g.nsu.ru>
2021-12-27 13:51:04 +0700

    Add calling event's handlers from anonymous function in search_songs.js

--- js/search_songs.js
+++ js/search_songs.js
@@ -1,62 +1,44 @@
-let filter = null;
+(function () {
+    let filter = null;
 
-$(document).ready(function () {
-    $(".dropdown-menu li a").click(function () {
-        setFilterValue(this.text);
-        document.getElementById("search_concept").textContent = this.text;
-    });
-});
-
-function ShowSearchedSongsOnPage(page)
-{
-    let search_by = document.getElementById("search_by").value;
-    if (search_by === null) {
-        search_by = '';
-    }
-
-    removeElementsByClass('audios-wrapper');
-    $.ajax({
-        type: 'POST',
-        url: 'find_song',
-        data: ({"condition": filter, "parameter": search_by, "page": page}),
-        success: function (audios_html) {
-            renderAudioElements(audios_html)
-        }
-    });
-}
-
-function ShowSongsListOnPage(page)
-{
-    //alert(search_by + '  ' + page + $filter);
-    removeElementsByClass('audios-wrapper');
-
-    $.ajax({
-        type: 'POST',
-        url: 'songs_list/page',
-        data: ({"page": page}),
-        success: function (audios_html) {
-            renderAudioElements(audios_html)
-        }
-    });
-}
-
-/*function ShowSearchedSongs()
-{
-    let search_by = document.getElementById("search_by").value;
-    if (search_by !== null && search_by !== '') {
-        removeElementsByClass('audios-wrapper');
-        $.ajax({
-            type: 'POST',
-            url: 'find_song',
-            data: ({"condition": filter, "parameter": search_by}),
-            success: function (audios_html) {
-                renderAudioElements(audios_html)
-            }
+    $(document).ready(function () {
+        $(".dropdown-menu li a").click(function () {
+            setFilterValue(this.text);
+            document.getElementById("search_concept").textContent = this.text;
         });
-    }
-}*/
 
-function setFilterValue(new_filter)
-{
-    filter = new_filter;
-}
+        let search_btn = document.getElementById('search_btn');
+        if (search_btn) {
+            search_btn.addEventListener("click", () => ShowSearchedSongsOnPage(1));
+        }
+
+        $('body').on('click', '.page-link', (event) => ShowSearchedSongsOnPage(event.target.textContent));
+
+    });
+
+    function ShowSearchedSongsOnPage(page)
+    {
+        let search_by = null;
+        if (document.getElementById("search_by")) {
+            search_by = document.getElementById("search_by").value;
+        }
+        if (search_by !== null && search_by !== '') {
+            removeElementsByClass('audios-wrapper');
+            $.ajax({
+                type: 'POST',
+                url: 'find_song',
+                data: ({"condition": filter, "parameter": search_by, "page": page}),
+                success: function (audios_html) {
+                    renderAudioElements(audios_html)
+                }
+            });
+        }
+    }
+
+    function setFilterValue(new_filter)
+    {
+        filter = new_filter;
+    }
+})();
+
+

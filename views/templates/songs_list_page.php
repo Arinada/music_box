5dcb925dba533959949af2f5dc189bc77f615187
Arinada <a.potekhina@g.nsu.ru>
2021-12-27 13:47:01 +0700

    Move calling js scripts at the end of the body in songs_list_page.php

--- views/templates/songs_list_page.php
+++ views/templates/songs_list_page.php
@@ -5,16 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="../../js/songs_list.js"></script>
-    <script src="../../js/common_functions.js"></script>
-    <link rel="stylesheet" href="../css/main_page/styles.css">
+    <link rel="stylesheet" href="http://192.168.56.2/css/main_page/styles.css">
 </head>
 <body>
 <?php
 include_once VIEW_DIR_PATH . '/templates/layouts/header.php';
 include_once VIEW_DIR_PATH . '/templates/blocks/sort_filters.php';
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="http://192.168.56.2/js/songs_list.js"></script>
+<script src="http://192.168.56.2/js/common_functions.js"></script>
 </body>
 </html>

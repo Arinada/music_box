762eb2526a39c6cb142685ab3b0c8116e7d4a9c0
Arinada <a.potekhina@g.nsu.ru>
2021-12-27 13:34:00 +0700

    Use search_songs.js to handle events on page with list of songs

--- views/templates/songs_list_page.php
+++ views/templates/songs_list_page.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="../../js/search_songs.js"></script>
+    <script src="../../js/songs_list.js"></script>
     <script src="../../js/common_functions.js"></script>
     <link rel="stylesheet" href="../css/main_page/styles.css">
 </head>

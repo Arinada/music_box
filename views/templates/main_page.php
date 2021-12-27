3a2e01af81d87fa61c0b798ab2d8c43c79de3e4f
Arinada <a.potekhina@g.nsu.ru>
2021-12-27 13:47:10 +0700

    Move calling js scripts at the end of the body in main_page.php

--- views/templates/main_page.php
+++ views/templates/main_page.php
@@ -5,11 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="../../js/search_songs.js"></script>
-    <script src="../../js/common_functions.js"></script>
-    <link rel="stylesheet" href="../css/main_page/styles.css">
+    <link rel="stylesheet" href="http://192.168.56.2/css/main_page/styles.css">
 </head>
 <body>
 
@@ -18,5 +14,9 @@ include_once VIEW_DIR_PATH . '/templates/layouts/header.php';
 include_once VIEW_DIR_PATH . '/templates/blocks/introduction_info.php';
 include_once VIEW_DIR_PATH . '/templates/blocks/search_panel.php';
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="http://192.168.56.2/js/search_songs.js"></script>
+<script src="http://192.168.56.2/js/common_functions.js"></script>
 </body>
 </html>

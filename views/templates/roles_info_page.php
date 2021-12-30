61cedf6d34306ac2be2cba50998cca063c663b3d
Arinada <a.potekhina@g.nsu.ru>
2021-12-30 14:30:43 +0700

    Add visual part of initial page with roles info in roles_info_page.php

---
+++ views/templates/roles_info_page.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <title>MusicBox</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://192.168.56.2/css/main_page/styles.css">
+</head>
+<body>
+<h1>Description rights for user</h1>
+<h2>Anonymous</h2>
+<p>blabla</p>
+<h2>Writer</h2>
+<p>blabla</p>
+<h2>Reader</h2>
+<p>blabla</p>
+</body>
+</html>
